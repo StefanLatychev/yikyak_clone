5b7cbb321564798f3d6bf8ec3e0de805fe7209ca
latychev <SLatychev@gmail.com>
2015-03-16 01:18:59 -0400

    Completed basic login and registration page layout. Finished basic control structure for login and registration, user can register account and log into Hello World main page

--- static/displayController.js
+++ static/displayController.js
@@ -1,70 +1,225 @@
-//Registration Controller
+//Display Controller
 
-
+/*******************************Constructors******************************/
 
 //Constructor for Registraction api request object
-function RegistrationAPIRequest(email, phone, password, confirm) {
-	this.email = email;
-	this.phone = phone;
-	this.password1 = password;
-	this.password2 = confirm;
+function packageRegistrationAPIRequest(email1, email2, phone, password, confirm) {
+	var request_payload = {
+		email1: email1,
+		email2: email2,
+		phone_number: phone,
+		password1: password,
+		password2: confirm
+	}
+	return request_payload;
 }
 
 //Constructor for Login api request object
-function LoginAPIRequest(username, password) {
-	this.email = username;
-	this.password = password;
+function packageLoginAPIRequest(username, password) {
+	var request_payload = {
+		email : username,
+		password : password
+	}
+	return request_payload;
 }
 
 //Add more constructors.
 
 
-//Registration validation, packaging, and sending as request to api.
-//Add more rigorous validation for inputs.
-$(function(){
+
+/*****************************Logic Functions******************************/
+
+//Initializes the page to the login screen
+function initialize() {
+	$('body > :not(#login)').hide();
 	$('.error').hide();
-	$(".button").click(function(){
-		//validate and process from here
-		$('.error').hide();
-		
-		//Validate if email was given
-		var email = $("input#email").val();
-		if(email == ""){
-			$("label#email_error").show();
-			$("input#email").focus();
-			return false;
-		}
-		
-		//Validate if phone was given then does it only contain allowed characters.
-		var phone = $("input#phone").val();
-		//Phone is not a required field
+	$('#login').appendTo('body');	
+}
 
-		//Validate if password was given
-		var password = $("input#password").val();
-		if(password == ""){
-			$("label#password_error").show();
-			$("input#password").focus();
-			return false;
-		}
 
-		//Validate if password confirmation was given and matches
-		var confirm = $("input#confirm").val();
-		if(confirm != password || confirm == ""){
-			$("label#confirm_error").show();
-			$("input#confirm").focus();
-			return false;
-		}
+//Changes display to specified page
+function changeDisplay(page_to_be_displayed) {
+	$('#'+page_to_be_displayed).show();
+	$('body > :not(#'+page_to_be_displayed+')').hide();
+	$('#'+page_to_be_displayed).appendTo('body');
+}
 
-		var dataString = JSON.stringify(RegistrationAPIRequest(email, phone, password, confirm));
-		//alert (dataString); return false;
-		$.ajax({
-			type: "POST",
-			url: "api/user.php", 
-			data: dataString,
-			success: function() {
-				//return user to login page
-			});
+
+
+/***************************Validation Functions****************************/
+
+//Login validation, packaging, and sending as request to api
+//TODO(SLatychev): Add more rigorous validation checking
+function loginValidation() {
+	$('.error').hide();
+
+	//Validation and field variables
+	var validated = true;
+	var email = $("input#login_email").val();
+	var password = $("input#login_password").val();
+
+	//Email Validation
+	if(email == ""){
+		$("label#login_email_error").show();
+		$("input#login_email").focus();
+		validated = false;
+	}
+	
+	//Password Validation
+	if(password == ""){
+		$("label#login_password_error").show();
+		$("input#login_password").focus();
+		validated = false;
+	}
+
+	//Validation check
+	if(!validated){
+		return false;
+	}
+	
+	//Package and send request
+	var payloadString = JSON.stringify(packageLoginAPIRequest(email, password));
+	sendAPIRequest("api/authentication.php", "POST", payloadString, loginValidated);
+	return false;
+}
+
+
+
+//Registration validation, packaging, and sending as request to api.
+//TODO(SLatychev): Add more rigorous validation checking
+function registerValidation(){
+	//Hide all error labels
+	$('.error').hide();
+	
+	//Validation and Field variables
+	var validated = true;
+	var email1 = $("input#email1").val();
+	var email2 = $("input#email2").val();
+	var phone = $("input#phone").val();
+	var password = $("input#password").val();
+	var confirm = $("input#confirm").val();
+
+	//Email Validation
+	if(email1 == ""){
+		$("label#email1_error").show();
+		$("input#email1").focus();
+		validated = false;
+	}	
+	if(email2 == ""){
+		$("label#email2_error").show();
+		$("input#email2").focus();
+		validated = false;
+	} else if(email2 != email1) {
+		$("label#email2_error2").show();
+		$("input#email2").focus();
+		validated = false;
+	}	
+	
+	//Phone Validation
+	var num = new RegExp("[0-9]*");
+	if(phone != "" && num.test(phone)) {
+		validated = false;
+	}
+	
+	//Password Validation
+	if(password == ""){
+	$("label#password_error").show();
+		$("input#password").focus();
+		validated = false;
+	}
+	if(confirm == ""){
+		$("label#confirm_error").show();
+		$("input#confirm").focus();
+		validated = false;
+	} else if(confirm != password) {
+		$("label#confirm_error2").show();
+		$("input#confirm").focus();
+		validated = false;
+	}
+	
+	//Validation Check
+	if(!validated) {
+		return false;
+	}
+
+	//Package and send request
+	var payloadString = JSON.stringify(packageRegistrationAPIRequest(email1, email2, phone, password, confirm));
+	sendAPIRequest("api/user.php", "POST", payloadString, registrationValidated);
+	return false;
+};
+
+
+
+/***************************Control Functions****************************/
+
+function registrationValidated(response_object) {
+	alert("Account has been registered.\nThank You!");
+	changeDisplay('login');
+	return false;
+}
+
+
+function loginValidated(response_object) {
+	alert("Logging you in");
+	changeDisplay('main');
+	return false;
+}
+
+
+/*
+ * Sends AJAX request to back-end and actions appropriately based on responce 
+ * status code.
+ */
+function sendAPIRequest(
+	api_request_url, 
+	http_request_method, 
+	encoded_request_payload, 
+	callback_func) {
+		//alert("Stepping into API Request");
+		var request = $.ajax({
+			url: api_request_url,
+			method: http_request_method,
+			data: {request : encoded_request_payload},
+			success: function(result) {		
+				// decode result (it is in json format)
+				result_object = JSON.parse(result);
+				
+					
+				alert("Parsed JSON object");
+				alert(result_object.status);
+				alert(STATUS_OK);
+				alert(Number(result_object.status) === STATUS_OK);
+				
+				
+				switch(Number(result_object.status)){
+					case STATUS_OK:
+						//Callback function goes here
+						callback_func(result_object);
+						//alert("Good Request");
+						//return result_object;
+						break;
+					case STATUS_BAD_REQUEST:
+						//alert("Bad Request");
+						break;
+					case STATUS_UNAUTHORIZED:
+						//Display info
+						break;
+					case STATUS_FORBIDDEN:
+						//Display info
+						break;
+					case STATUS_REQUEST_URI_TOO_LONG:
+						//Display info
+						break;
+					case STATUS_INTERNAL_SERVER_ERROR:
+						//Display info
+						break;
+					case STATUS_NOT_IMPLEMENTED:
+						//Display info
+						break;
+					default:
+						//Do default action
+			}
 		}
 	});
-	return false;
-});
+}
+
